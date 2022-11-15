85a0ead9553701f3b9af90e6bbd3374ac03202b0
Noorbegaum K Hussain <noorbegaumkhussain@Noor-iMac.local>
2022-11-15 18:27:32 +0530

    added AddPlayer,CreateTournamentSuccess,AddTeam screens

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import UserControls from '../screens/UserControls';
-import LoginScreen from '../screens/LoginScreen';
-
+// import UserControls from '../screens/UserControls';
+// import LoginScreen from '../screens/LoginScreen';
+import CreateTournament from '../screens/CreateTournament';
+import CreateTournamentSuccess from '../screens/CreateTournamentSuccess';
+import TeamsList from '../screens/TeamsList';
 const Stack = createNativeStackNavigator();
 
 const AppStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
+        {/* <Stack.Screen
           name="UserControls"
           component={UserControls}
+          options={{headerShown: false}}
+        /> */}
+        <Stack.Screen
+          name="CreateTournament"
+          component={CreateTournament}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="CreateTournamentSuccess"
+          component={CreateTournamentSuccess}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="TeamsList"
+          component={TeamsList}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
---
+++ src/screens/AddPlayer.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddPlayer = () => {
+  return (
+    <View>
+      <Text>AddPlayer</Text>
+    </View>
+  )
+}
+
+export default AddPlayer
