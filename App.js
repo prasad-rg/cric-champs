df4275de123d9727f260c8aa571a99e19473109e
Noorbegaum K Hussain <noorbegaumkhussain@Noor-iMac.local>
2022-11-18 18:17:15 +0530

    added screens to navigation

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { View, Text,StyleSheet } from 'react-native'
-import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import OnBoardingStack from './src/navigation/OnBoardingStack';
+import CreateTournament from './src/screens/CreateTournament';
+import AppStack from './src/navigation/AppStack';
+import AddTeam from './src/screens/AddTeam';
+import store from './src/redux/store';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
+import ProfileImagePicker from './src/components/ProfileImagePicker';
+
+let persistor = persistStore(store);
 
 const App = () => {
   return (
-    <View style={{flex:1,justifyContent:'center', alignItems:'center'}}>
-      <Text style={styles.text}>App</Text>
-    </View>
-  )
-}
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <AppStack />
+      </PersistGate>
+    </Provider>
+    // <ProfileImagePicker/>
+  );
+};
 
-export default App
-
-const styles = StyleSheet.create({
-  text:{
-    fontFamily:'HelveticaNeue Bold'
-  }
-})
+export default App;
