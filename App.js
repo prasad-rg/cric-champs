94482837dccfdf7c9a112ac068bb1d8f1ce697f4
Prasad R G <prasadrg@PrasadRG-iMac.local>
2022-11-18 18:22:05 +0530

    Added App.jks

--- App.js
+++ App.js
@@ -4,6 +4,14 @@ import 'react-native-gesture-handler';
 import AuthStack from './src/navigation/AuthStack';
 import CreateTournament from './src/screens/CreateTournament';
 import UserControl from './src/screens/UserControl';
+import store from './src/redux/store';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
+import OnBoardingStack from './src/navigation/OnBoardingStack';
+import RootNavigator from './src/navigation/RootNavigator';
+
+let persistor = persistStore(store);
 
 const App = () => {
   // return (
@@ -18,7 +26,14 @@ const App = () => {
 
   //   </View>
   // );
-  return <AuthStack />;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        {/* <AuthStack /> */}
+        <RootNavigator />
+      </PersistGate>
+    </Provider>
+  );
   // return <UserControl />;
 };
 
