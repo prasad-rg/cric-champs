a64b903f24b1b8c04bf4e865d0a4c4453534d994
Ashwitha Shetty <ashwithashetty@S-iMac.local>
2022-11-19 12:39:34 +0530

    Added profile details

--- src/screens/PlayerProfile.js
+++ src/screens/PlayerProfile.js
@@ -1,29 +1,125 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import AddProfileDetails from '../components/AddProfileDetails'
+import {View, StyleSheet, Text, Alert, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+import AddProfileDetails from '../components/AddProfileDetails';
+import {TextField} from 'rn-material-ui-textfield';
+import GradientButton from '../components/GradientButton';
+import RadioButton from '../components/RadioButton';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import {Formik} from 'formik';
+import * as yup from 'yup';
 
 const PlayerProfile = ({navigation}) => {
-    const [profilePictureUri, setProfilePictureUri] = useState('');
-    const getDetails = data => {
-        setProfilePictureUri(data);
-      };
+  const [gender, setGender] = useState('');
+  const [profilePictureUri, setProfilePictureUri] = useState('');
+  const getDetails = data => {
+    setProfilePictureUri(data);
+  };
 
+  const Details = [
+    {
+      id: 1,
+      title: 'City / Town',
+      value: 'Delhi',
+    },
+    {
+      id: 2,
+      title: 'Team',
+      value: 'Paras XI',
+    },
+    {
+      id: 3,
+      title: 'Captain',
+      value: 'Yes',
+    },
+    {
+      id: 4,
+      title: 'Role',
+      value: 'batsman / Captain',
+    },
+    {
+      id: 5,
+      title: 'Batting Style',
+      value: 'Right Handed',
+    },
+    {
+      id: 6,
+      title: 'Bowling Style',
+      value: 'Right-arm medium',
+    },
+    {
+      id: 7,
+      title: 'Matches',
+      value: '5',
+    },
+    {
+      id: 8,
+      title: 'Runs',
+      value: '151',
+    },
+    {
+      id: 9,
+      title: 'Wickets',
+      value: '4',
+    },
+    {
+      id: 10,
+      title: 'Achievements',
+      value: 'Man of the Match (Match 5)',
+    },
+  ];
   return (
-    <View style={styles.container}>
-        {/* <AddProfileDetails profilePictureUri = {require('../../assets/images/profile2.png')}
-  backroundImageUri = {require('../../assets/images/umpire.png')} title='Add Umpire'/> */}
-              <AddProfileDetails
-              backroundImageUri={require('../../assets/images/ground1.png')}
-              title="Add Player"
-              navigation={navigation}
-              getImageUri={getDetails}
-            />
-        </View>
-  )
-}
-
-export default PlayerProfile
+    <View style={styles.primaryContainer}>
+        <ScrollView>
+      <AddProfileDetails
+        navigation={navigation}
+        title="James Arthur"
+        getImageUri={getDetails}></AddProfileDetails>
+      <View>
+        {Details.map(item => (
+          <View key={item.id} style={styles.listview}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.value}>{item.value}</Text>
+          </View>
+        ))}
+      </View>
+      </ScrollView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  flex:1
-})
+  container: {
+    flex: 1,
+    paddingHorizontal: 50,
+  },
+  primaryContainer: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  listview: {
+    padding: 20,
+    flexDirection: 'row',
+  },
+  title: {
+    height: 24,
+
+    color: 'rgba(77,77,77,0.87)',
+    fontFamily: 'Roboto-Regular',
+    fontSize: 14,
+    letterSpacing: 0,
+    lineHeight: 24,
+  },
+  value: {
+    height: 24,
+    width: '50%',
+    color: 'rgba(77,77,77,0.87)',
+    fontFamily: 'Roboto-Medium',
+    fontSize: 14,
+    fontWeight: '500',
+    letterSpacing: 0,
+    lineHeight: 24,
+    marginLeft: 'auto',
+  },
+});
+
+export default PlayerProfile;
