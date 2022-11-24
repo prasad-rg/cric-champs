3fc51a6dbf277ae86df8a94a06edd09cee57b975
Ashwitha Shetty <ashwithashetty@S-iMac.local>
2022-11-24 12:25:04 +0530

    app stack changed

--- src/services/manageTournament.js
+++ src/services/manageTournament.js
@@ -1,5 +1,5 @@
 import {BASE_URL} from '../api/baseURL';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzdjOTg2NjViZDk4YTJmOThiZTA3M2UiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2NjkyMDc2NDMsImV4cCI6MTY2OTIxMTI0M30.1rDwkAot2S8I6OTaORJjfejOvRjzJdQfaFU2_f8NFsE';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzdjOTg2NjViZDk4YTJmOThiZTA3M2UiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2NjkyNzIxMjksImV4cCI6MTY2OTI3NTcyOX0.nG0AXFpAl-vrbsW79XbC9sfyfncj1KMUH7Ztpv1Upfg';
 
 export const createTournament = async formData => {
   let res = await fetch(`${BASE_URL}/api/tournament`, {
--- src/services/manageTournament2.js
+++ src/services/manageTournament2.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {BASE_URL} from '../api/baseURL';
 
 const token =
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzdjOTg2NjViZDk4YTJmOThiZTA3M2UiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2NjkyNjkwNTcsImV4cCI6MTY2OTI3MjY1N30.AEyAsXp-gTTV_0qDaoHG7xYUxGQVJg-0SWZE7XHQaYs';
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzdjOTg2NjViZDk4YTJmOThiZTA3M2UiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2NjkyNzIxMjksImV4cCI6MTY2OTI3NTcyOX0.nG0AXFpAl-vrbsW79XbC9sfyfncj1KMUH7Ztpv1Upfg';
 
 export const addOvers = async oversData => {
   try {
