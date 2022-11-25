9a301b485af2f0661b21f054981f4187841d4447
Noorbegaum K Hussain <noorbegaumkhussain@Noor-iMac.local>
2022-11-25 09:19:55 +0530

    made token globally accessible

--- src/services/manageTournament2.js
+++ src/services/manageTournament2.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import {BASE_URL} from '../api/baseURL';
+import { token } from '../api/token';
 
-const token =
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzdjOTg2NjViZDk4YTJmOThiZTA3M2UiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2NjkyOTMzNTYsImV4cCI6MTY2OTI5Njk1Nn0.mNfvNUrUM7PniFCvZi6TV-eq4p3JDC8jwApju1jizNs';
+
+// const token =
+// 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzdjOTg2NjViZDk4YTJmOThiZTA3M2UiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2NjkyOTMzNTYsImV4cCI6MTY2OTI5Njk1Nn0.mNfvNUrUM7PniFCvZi6TV-eq4p3JDC8jwApju1jizNs';
 
 export const addOvers = async oversData => {
   try {
--- src/services/tournamentManagement.js
+++ src/services/tournamentManagement.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import {BASE_URL} from '../api/baseURL';
+import { token } from '../api/token';
 
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzdjOTg2NjViZDk4YTJmOThiZTA3M2UiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2NjkyODU3MDksImV4cCI6MTY2OTI4OTMwOX0.e74fZj2TjdvGCC603Q7Wmy7Zk_vbuBEMBzNCaJJGjIY';
+// const token =
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MzdjOTg2NjViZDk4YTJmOThiZTA3M2UiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE2NjkyODU3MDksImV4cCI6MTY2OTI4OTMwOX0.e74fZj2TjdvGCC603Q7Wmy7Zk_vbuBEMBzNCaJJGjIY';
 
 export const tournamentOverview = async tournamentId => {
   try {
