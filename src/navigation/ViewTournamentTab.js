92607becc6233155785db7a5528440b1fc22f852
Ashwitha Shetty <ashwithashetty@S-iMac.local>
2022-11-29 11:33:13 +0530

    changes in view tab

--- src/navigation/ViewTournamentTab.js
+++ src/navigation/ViewTournamentTab.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet} from 'react-native';
 import React from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {NavigationContainer} from '@react-navigation/native';
@@ -60,6 +60,3 @@ const ViewTournamentTab = () => {
 
 export default ViewTournamentTab;
 
-const styles = StyleSheet.create({});
-
-//  options={{headerShown: false}}
